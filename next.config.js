132f8193c0e7c9dafb6e90b856f079754d21e27b
Geraldine Martinez <geralmartinez000@gmail.com>
2025-02-03 14:18:17 -0300

    build

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export",
+    assetPrefix: "/asytec-web-produ",
+    basePath: "/asytec-web-produ",
+};
+  
+module.exports = nextConfig;
+  
